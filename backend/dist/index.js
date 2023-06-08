55b74bdf48cd8fd03f32d6ac50a6140ca808fc96
Nagalin punyakalin <nagalin.pun@dome.tu.ac.th>
2023-06-08 17:34:35 +0700

    setup typescipt for backend

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,6 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs_1 = __importDefault(require("fs"));
-fs_1.default;
-//# sourceMappingURL=index.js.map
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('Hello from Tssd');
+});
+app.listen(8000, () => console.log('Listenning on port 8000'));
---
+++ backend/index.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send('Hello from Tssd')
+})
+
+
+app.listen(8000,()=> console.log('Listenning on port 8000'))
